9d03e33cfc2ac5fa2c6ff22e700f6dad95304f35
LHaug <lucas.haug@hotmail.com>
2021-11-15 19:42:39 -0300

    ♻️ Refactor question 1

--- data.py
+++ data.py
@@ -13,26 +13,7 @@ def get_data(file_name):
 
     data = pd.read_csv(file_name)
     features = data.filter(like="x", axis=1).to_numpy()
-    target = data.filter(like="target", axis=1).to_numpy()
+    target = data.filter(like="target", axis=1).to_numpy().ravel()
 
     return features, target
 
-
-def get_holdout_data(file_name, holdout_size):
-    """
-    Reads the data from the file and returns the training
-    and test sets using the holdout method.
-
-    Args:
-        file_name (str): The name of the file to be read.
-        holdout_size (int): The size of the holdout set.
-    """
-
-    features, target = get_data(file_name)
-
-    x_train = features[:-holdout_size]
-    y_train = target[:-holdout_size].ravel()
-    x_test = features[-holdout_size:]
-    y_test = target[-holdout_size:].ravel()
-
-    return x_train, y_train, x_test, y_test
--- question1.py
+++ question1.py
@@ -15,12 +15,34 @@ Author
     Lucas Haug <lucas.haug@usp.br>
 """
 
-from data import get_holdout_data
+from data import get_data
 from sklearn.naive_bayes import GaussianNB
 
 
+def get_holdout_data(file_name, holdout_size):
+    """
+    Reads the data from the file and returns the training
+    and test sets using the holdout method.
+
+    Args:
+        file_name (str): The name of the file to be read.
+        holdout_size (int): The size of the holdout set.
+    """
+
+    features, target = get_data(file_name)
+
+    x_train = features[:-holdout_size]
+    y_train = target[:-holdout_size].ravel()
+    x_test = features[-holdout_size:]
+    y_test = target[-holdout_size:].ravel()
+
+    return x_train, y_train, x_test, y_test
+
+
 def main():
-    x_train, y_train, x_test, y_test = get_holdout_data("data/class01.csv", holdout_size=650)
+    x_train, y_train, x_test, y_test = get_holdout_data(
+        "data/class01.csv", holdout_size=650
+    )
 
     clf = GaussianNB()
     clf.fit(x_train, y_train)
